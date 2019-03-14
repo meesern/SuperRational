170855cd505b27effb69b0c90a7fd45f5a560b62
Rupert <meesern@lucy.lan>
2019-03-14 19:06:56 +0000

    Changed box colour

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -35,9 +35,9 @@ export const IndexPageTemplate = ({
       <h1
         className="has-text-weight-bold is-size-3-mobile is-size-2-tablet is-size-1-widescreen"
         style={{
-          boxShadow: 'rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px',
+          boxShadow: 'rgb(255, 102, 0) 0.5rem 0px 0px, rgb(255, 102, 0) -0.5rem 0px 0px',
           borderRadius: '10px',
-          backgroundColor: 'rgb(255, 68, 0)',
+          backgroundColor: 'rgb(255, 102, 0)',
           color: 'white',
           lineHeight: '1',
           padding: '0.25em'
@@ -47,9 +47,9 @@ export const IndexPageTemplate = ({
       </h1>
       <h3 className="has-text-weight-bold is-size-5-mobile is-size-5-tablet is-size-4-widescreen"
           style={{
-            boxShadow: 'rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px',
+            boxShadow: 'rgb(255, 102, 0) 0.5rem 0px 0px, rgb(255, 102, 0) -0.5rem 0px 0px',
             borderRadius: '10px',
-            backgroundColor: 'rgb(255, 68, 0)',
+            backgroundColor: 'rgb(255, 102, 0)',
             color: 'white',
             lineHeight: '1',
             padding: '0.25em'
